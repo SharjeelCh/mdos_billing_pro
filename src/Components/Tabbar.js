b943d1c85370d8f73b6922732c1d57bf2c69c661
sharjeelch <sharjeelh6451@gmail.com>
2024-08-01 01:15:26 -0700

    ..

--- src/Components/Tabbar.js
+++ src/Components/Tabbar.js
@@ -3,12 +3,18 @@ import "../Styles/Tabbar.css";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 import { MenuBook, MenuOpen, MenuRounded } from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import { MessageOutlined, UserOutlined } from "@ant-design/icons";
+import "../Styles/Appp.css";
+
+import { Avatar, Flex, Typography } from "antd";
 function Tabbar() {
   const [show, setshow] = useState(false);
+  const user = useSelector((state) => state.auth.user);
   const nav = useNavigate();
   return (
     <header className="header">
-      <Link to={"/Dashboard"} className="logo">
+      <Link to={"/"} className="logo">
         pleo
       </Link>
       <button
@@ -44,17 +50,28 @@ function Tabbar() {
         </a>
       </nav>
       <div className="auth">
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2, backgroundColor: "#040365" }}
-          onClick={() => {
-            nav("/Login");
-          }}
-        >
-          Login
-        </Button>
+        {!user ? (
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor: "#040365" }}
+            onClick={() => {
+              nav("/Login");
+            }}
+          >
+            Login
+          </Button>
+        ) : (
+          <Flex align="flex-end" gap={"0.6rem"}>
+            <Typography.Title level={5} type="secondary">
+              {user.name}
+            </Typography.Title>
+            <Link to={"/Dashboard"}>
+              <Avatar icon={<UserOutlined />} size="large" className="avatar" />
+            </Link>
+          </Flex>
+        )}
       </div>
     </header>
   );
